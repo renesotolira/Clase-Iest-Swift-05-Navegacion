274db61be9b21fa6969259f93c9275ced6d3a719
René <rene.soto01@iest.edu.mx>
2023-09-25 14:08:23 -0600

    Initial Commit

---
+++ Navegacion/NavegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionApp.swift
+//  Navegacion
+//
+//  Created by Rene SL on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NavegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
